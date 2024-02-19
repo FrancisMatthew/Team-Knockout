82d763cd8d7aadabcbb8d2071d67ebe76cdc486e
TrippieEclipse <na899@student.aru.ac.uk>
2024-02-19 14:47:20 +0000

    combat system refine
    combat system has been refined, including knockback. further addition will be implemented. more animations need to be added

--- Code Duello/Assets/Scripts/Nate/PlayerHealthClass.cs
+++ Code Duello/Assets/Scripts/Nate/PlayerHealthClass.cs
@@ -1,10 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+public enum HitType 
+{ 
+    Head,
+    Chest,
+    Legs
+}
+
 
 public class PlayerHealthClass : MonoBehaviour
 {
+    public MovementController playerMC;
+
     public float health = 25f;
 
+    public Slider healthSlider;
+
+    [SerializeField] private float damageMultiplyerChest = 1;
+    [SerializeField] private float damageMultiplyerHead = 1.5f;
+
+    private void Update()
+    {
+        healthSlider.value = health;
+    }
+
+    public void TakeDamage(HitType hitType, float damageIntake) 
+    {
+        if (playerMC.isBlocking)
+        {
+            BockHit();
+            return;
+        }
+
+        if (hitType == HitType.Head) 
+        {
+            Debug.Log("HIT: " + hitType);
+            health -= damageIntake * damageMultiplyerHead;
+            Debug.Log(damageIntake * damageMultiplyerHead);
+            playerMC.ApplyKnockback();
+        }
+        if(hitType == HitType.Chest) 
+        {
+            
+            Debug.Log("HIT: " + hitType);
+            health -= damageIntake * damageMultiplyerChest;
+            Debug.Log(damageIntake * damageMultiplyerChest);
+            playerMC.ApplyKnockback();
+        }
+        
+    }
+
+    public void BockHit() 
+    { 
+    
+    }
 
 }
